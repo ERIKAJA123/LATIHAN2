5ba169a6083b182270d87174fd474300c98f8df2
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 14:44:04 +0700

    test

---
+++ Pertemuan2/src/main/kotlin/latihan2.kt
@@ -0,0 +1,18 @@
+fun main (args: Array<String>){
+
+    var namalengkap: String = "Erik"
+    var alamat: String
+    alamat = "sumatra selatan"
+    var umur = 20
+
+    println(namalengkap)
+    println(alamat)
+    println(umur)
+
+    var namaLengkap ="Erik"
+    namaLengkap ="Erika Ramadona"
+
+    println(namaLengkap)
+    val nik = 987372
+    println(nik)
+}
